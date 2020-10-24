980d88e26e6656b73c3046bbd8f5f5c8133b1104
alekchr042 <o.chrzaszcz@gmail.com>
2020-10-24 18:40:59 +0200

    Some fixes to project card UI

--- DIYManager/ClientApp/src/app/project-card/project-card.component.ts
+++ DIYManager/ClientApp/src/app/project-card/project-card.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
-import { Project } from '../models/project';
-import { DatePipe } from '@angular/common';
+import { Component, OnInit, Input, ElementRef, ViewChild } from "@angular/core";
+import { Project } from "../models/project";
+import { DatePipe } from "@angular/common";
+import { faCrow } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-project-card',
-  templateUrl: './project-card.component.html',
-  styleUrls: ['./project-card.component.css']
+  selector: "app-project-card",
+  templateUrl: "./project-card.component.html",
+  styleUrls: ["./project-card.component.css"],
 })
 export class ProjectCardComponent implements OnInit {
-
-  @ViewChild('projectThumbnail', { static: true }) projectThumbnail: ElementRef;
+  @ViewChild("projectThumbnail", { static: true }) projectThumbnail: ElementRef;
 
   @Input() project: Project;
-  constructor(private datePipe: DatePipe) { }
+  faCrow = faCrow;
+
+  constructor(private datePipe: DatePipe) {}
 
   get lastModifiedDate() {
-    var formatted = this.datePipe.transform(this.project.lastModified, 'yyyy-MM-dd');
+    var formatted = this.datePipe.transform(
+      this.project.lastModified,
+      "yyyy-MM-dd"
+    );
     return formatted;
   }
 
-  isThumbnailAttached() {
-    var result = this.project.thumbnail != null && this.project.thumbnail.length > 0;
+  get isThumbnailAttached() {
+    var result =
+      this.project.thumbnail != null && this.project.thumbnail.length > 0;
     return result;
   }
 
@@ -29,12 +35,12 @@ export class ProjectCardComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.projectThumbnail.nativeElement.src = "data:image/png;base64," + this.getFileContent();
+    this.projectThumbnail.nativeElement.src =
+      "data:image/png;base64," + this.getFileContent();
   }
 
   formatDate(date: Date) {
     var result = date.toLocaleDateString("en-US");
     return result;
   }
-
 }
